83b3e2e1387fdfae5a07454bd15b3c521077fa22
12399nhan <nle226208@gmail.com>
2025-01-25 19:07:58 +0700

    Add files via upload

---
+++ NQM DZ Hub PREMIUM MOBILE AND PC V4 .lua
@@ -0,0 +1,15 @@
+repeat task.wait() until game:IsLoaded()
+local TablePlace = {7449423635,2753915549,4442272183}
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "NQM DZ V4", -- Required
+	Text = "Wait...", -- Required
+	Icon = "rbxassetid://133155358192674", -- Optional
+	Duration = 15
+})
+if table.find(TablePlace,game.PlaceId) then
+    getgenv().Game = "BF"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/UserDevEthical/Loadstring/main/BF-New.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/UserDevEthical/other/refs/heads/main/secure_call.lua"))()
+else
+    game.Players.LocalPlayer:Kick("not Support")
+end
